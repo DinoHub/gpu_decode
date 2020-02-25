2b2a304a15ff9ed4b58adc20e9ace8694f0b181c
= <dstadh17@gmail.com>
2020-02-25 10:10:28 +0800

    added xhost stuffs

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,2 @@
-sudo docker run -it --gpus all -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video  -v $HOME/Workspace:/workspace -v $HOME/Videos:/videos  --ipc host --env="DISPLAY"  levan92/vision-suite-gpu
+xhost local:root
+sudo docker run -it --gpus all -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video  -v $HOME/Workspace:/workspace -v $HOME/Videos:/videos  --net=host --ipc=host --env="DISPLAY"  levan92/vision-suite-gpu
