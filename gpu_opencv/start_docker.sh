e9033964e165a16eb09fa3ae2e149d39bbb5ce0e
levan92 <lingevan0208@gmail.com>
2020-10-19 14:38:05 +0800

    run docker to display

--- gpu_opencv/start_docker.sh
+++ gpu_opencv/start_docker.sh
@@ -3,4 +3,4 @@ export WORKSPACE=$HOME/Workspace
 export DATA=/media/dh/HDD/
 
 xhost +local:docker
-docker run -it  --gpus all -w $WORKSPACE -v $WORKSPACE:$WORKSPACE -v $DATA:$DATA  opencv_gpu:rtx2070
+docker run -it  --gpus all -w $WORKSPACE -v $WORKSPACE:$WORKSPACE -v $DATA:$DATA --net=host --ipc host -e DISPLAY=unix$DISPLAY  opencv_gpu:rtx2070
